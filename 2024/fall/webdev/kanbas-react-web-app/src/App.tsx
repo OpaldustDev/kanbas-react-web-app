6341a6b3984e376d05efaa066523d950a3442f91
BleuSky03 <taksin@photomann.com>
2024-09-24 18:14:14 -0400

    dashboard and courses

--- 2024/fall/webdev/kanbas-react-web-app/src/App.tsx
+++ 2024/fall/webdev/kanbas-react-web-app/src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Labs from "./Labs";
+import Kanbas from "./Kanbas";
 import { HashRouter, Route, Routes, Navigate } from "react-router-dom";
 
 const App = () => {
@@ -7,8 +8,9 @@ const App = () => {
         <HashRouter>
             <div>
                 <Routes>
-                    <Route path="/" element={<Navigate to="Labs" />} />
+                    <Route path="/" element={<Navigate to="Kanbas" />} />
                     <Route path="/Labs/*" element={<Labs />} />
+                    <Route path="/Kanbas/*" element={<Kanbas />} />
                 </Routes>
             </div>
         </HashRouter>
