3572d4fa92ec3323197efbcd7929d482b6ffc7bb
BleuSky03 <taksin@photomann.com>
2024-09-24 21:51:46 -0400

    redirect to landing page

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -6,7 +6,7 @@ import Lab3 from "./Lab3";
 export default function Labs() {
     return (
         <div>
-            <h1>Taksin Mann</h1>
+            <h1>Taksin Mann | Section 2</h1>
             <h1>Labs</h1>
             <TOC />
             <Routes>
